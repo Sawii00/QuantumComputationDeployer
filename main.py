f2a73c3c91f206c0b029ec43189f811ebd26d511
Sawii00 <sawii00@sawiiDebian.com.sawii>
2021-06-26 15:24:16 +0200

    Added TODOs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.simulation import *
 import netsquid as ns
 import qiskit as qs
+import sys
 from netsquid_netconf.builder import ComponentBuilder
 from netsquid_netconf.netconf import netconf_generator
 
@@ -9,14 +10,12 @@ def main():
     conf = input("Insert YAML file: ")
     code = input("Insert QASM file: ")
 
-    if conf == "":
-        conf = "tests/test3.yaml"
-
-    if code == "":
-        code = "tests/test3.qasm"
+    if conf == "" or code == "":
+        print("Insert valid input files")
+        sys.exit(1)
 
     # In order for the component builder to recognize our custom classes, we must add them to its dictionary.
-    #ComponentBuilder.add_type("classical_connection", ClassicalDirectConnection)
+    # ComponentBuilder.add_type("classical_connection", ClassicalDirectConnection)
     ComponentBuilder.add_type("quantum_connection", QuantumDirectConnection)
     ComponentBuilder.add_type("quantum_node", QuantumNode)
     # Invokes the network generator which takes nodes and connections and sets them up within a network object.
@@ -68,15 +67,14 @@ def main():
 if __name__ == "__main__":
     main()
 
-# TODO(sawii)
+
+
 '''
-
-
-##TO BE TALKED ABOUT IN THE THESIS REGARDLESS
+@TODO(Sawii):
 - Remotely controlled operations
-- Entanglement Swapping 
-    
-#IN THE FUTURE
+- Entanglement Swapping
+- Sequential Execution of Chunks of Code
+    --> Maybe a gate "wait" at which both involved nodes must wait for the other before continuing with the simulation
 - implement all instructions also with custom parameters
 - make channels more flexible to allow specification of loss and delay from yaml
 - support multiple qubits instruction from qasm (X q[0,2,3])
